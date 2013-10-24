7af5e757454a8a709907538552216a9d9ee18856
Tyler Garlick <tjgarlick@gmail.com>
2013-10-24 12:04:34 -0400

    Fixed Run.IgnoreExceptions

---
+++ Pims.Security.Client/Framework/Run.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Streamline.Pims.Security.Client.Framework
+{
+    public static class Run
+    {
+        public static void IgnoreExceptions(Action code)
+        {
+            try
+            {
+                code();
+            }
+            catch
+            {
+            }
+        }
+    }
+}
